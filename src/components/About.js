1617b747235da3c9d79c8fb5d8a5da811a70e328
Manish Baghel <manish_11914130@nitkkr.ac.in>
2022-09-27 19:06:45 +0530

    implementation of state in the front end started

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,8 @@
-import { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/NoteContext'
+
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-    a.update();
-  }, [])
-  
   return (
-    <div>This is about {a.state.name} and he is class {a.state.class}</div>
+    <div>This is about</div>
   )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,22 +1,54 @@
+// import { useState } from "react";
 import { useState } from "react";
 import noteContext from "./NoteContext";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "manish",
-        "class" : "5b"
-    }
-    const [state, setstate] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setstate({
-                "name" : "Sonu",
-                "class" : "10b"
-            });
-        },10000);
-    }
+    const notesInitial = [
+        {
+            "_id": "6332ee2a2889f95d4c1d3fe7",
+            "user": "633033f4a4d8809a8ec9be47",
+            "title": "Meeting scheduled for 2 pm today",
+            "description": "Please do the work on time for the meeting today",
+            "tag": "team",
+            "__v": 0
+        },
+        {
+            "_id": "6332ee622889f95d4c1d3feb",
+            "user": "633033f4a4d8809a8ec9be47",
+            "title": "New meeting at Hotel Blue Orchids",
+            "description": "event to attend today",
+            "tag": "team",
+            "__v": 0
+        },
+        {
+            "_id": "6332ee662889f95d4c1d3fed",
+            "user": "633033f4a4d8809a8ec9be47",
+            "title": "Physics",
+            "description": "cooking is necessary",
+            "tag": "team",
+            "__v": 0
+        },
+        {
+            "_id": "6332ee692889f95d4c1d3fef",
+            "user": "633033f4a4d8809a8ec9be47",
+            "title": "current work is pending",
+            "description": "we have a deadline on Monday",
+            "tag": "team",
+            "__v": 0
+        },
+        {
+            "_id": "6332ee6d2889f95d4c1d3ff1",
+            "user": "633033f4a4d8809a8ec9be47",
+            "title": "todo list",
+            "description": "work last week",
+            "tag": "team",
+            "__v": 0
+        }
+    ]
+    const [notes, setNotes] = useState(notesInitial)
+
     return (
-        <noteContext.Provider value = {{state, update}}>
+        <noteContext.Provider value={{ notes, setNotes }}>
             {props.children}
         </noteContext.Provider>
     )
